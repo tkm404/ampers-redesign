6263df24825f5372aace6774275ed4335be2123f
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-19 15:50:54 -0800

    add HomeControl route to src/components/app.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,17 @@ import Header from './Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from '../logo.svg';
 import './App.css';
+import HomeControl from './HomeControl';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        {/* <Route path='/' element={<HomeControl />} /> */}
-        {/* <Route path='/' element={ServicesControl />} /> */}
-        {/* <Route path='/' element={TeamControl />} /> */}
-        {/* <Route path='/' element={ContactControl />} /> */}
+        <Route path='/' element={<HomeControl />} />
+        {/* <Route path='/services' element={ServicesControl />} /> */}
+        {/* <Route path='/team' element={TeamControl />} /> */}
+        {/* <Route path='/contact' element={ContactControl />} /> */}
       </Routes>
     </Router>
   );
