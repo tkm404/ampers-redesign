2e1d2e2157e14142eab493c1d0430e7453e3461d
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-19 10:56:17 -0800

    add imports from Header and react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import Header from './Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from '../logo.svg';
 import './App.css';
 
