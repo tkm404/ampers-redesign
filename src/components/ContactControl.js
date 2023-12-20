2bdd0fb8f2fd2cd910f66c5a85d8a511d1e2b704
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-20 12:59:51 -0800

    import Banner comp to ContactControl

--- src/components/ContactControl.js
+++ src/components/ContactControl.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Stack from 'react-bootstrap/Stack'
 import Contact from '../pages/Contact'
+import Banner from './Banner';
 
 
 function ContactControl() {
@@ -9,6 +10,7 @@ function ContactControl() {
       <Stack gap={2} className="p-4 col-md-8 mx-auto">
         <Contact />
       </Stack>
+      <Banner />
     </React.Fragment>
   )
 }
