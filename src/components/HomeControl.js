e9ded881130250eb9a3e414639a2f18724399197
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-20 13:01:19 -0800

    import Banner and Carousel...
    change order of display to stack without the banner becoming a footer

--- src/components/HomeControl.js
+++ src/components/HomeControl.js
@@ -3,19 +3,34 @@ import Button from 'react-bootstrap/Button'
 import Stack from 'react-bootstrap/Stack'
 import Container from 'react-bootstrap/Container'
 import Home from '../pages/Home'
+import Banner from './Banner'
+import HomeCarousel from './HomeCarousel'
 
 function HomeControl() {
   let currentlyVisibleState = 
     <Home />;
 
   return (
-    <React.Fragment>
-    
-      <Stack gap={2} className="p-4 col-md-8 mx-auto">
-      {currentlyVisibleState}          
-      </Stack>
+    <div>
+      <div> 
+        <React.Fragment>
+          <Stack gap={2} className="p-4 col-md-8 mx-auto">
+            {currentlyVisibleState} 
+          </Stack>
 
-    </React.Fragment>
+        <Banner />
+
+      </React.Fragment>
+      </div>
+
+      <div>
+        <div className='p-5'>
+          <HomeCarousel />  
+        </div>
+      </div>
+
+    </div>
+
   )
 }
 
