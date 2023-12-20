11ddf4c8193fa6e164af5d1157a8e970c5d35f70
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-20 13:00:07 -0800

    import Banner to ServicesControl

--- src/components/ServicesControl.js
+++ src/components/ServicesControl.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Services from '../pages/Services'
 import Stack from 'react-bootstrap/Stack'
+import Banner from "./Banner";
 
 
 function ServicesControl() {
@@ -10,6 +11,7 @@ function ServicesControl() {
       <Stack gap={2} className="p-4 col-md-8 mx-auto">
         <Services />
       </Stack>
+      <Banner />
     </React.Fragment>
   )
 }
