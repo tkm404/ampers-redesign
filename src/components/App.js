8023f255904c86d391955940dc0f740d75da94a7
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-20 12:17:44 -0800

    Add Banner import, enable routes, fix route typos.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Banner from './Banner';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import logo from '../logo.svg';
 import './App.css';
 import HomeControl from './HomeControl';
+import ServicesControl from './ServicesControl'
+import TeamControl from './TeamControl'
+import ContactControl from './ContactControl'
 
 function App() {
   return (
@@ -11,10 +15,11 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<HomeControl />} />
-        {/* <Route path='/services' element={ServicesControl />} /> */}
-        {/* <Route path='/team' element={TeamControl />} /> */}
-        {/* <Route path='/contact' element={ContactControl />} /> */}
+        <Route path='/services' element={<ServicesControl />} />
+        <Route path='/team' element={<TeamControl />} />
+        <Route path='/contact' element={<ContactControl />} />
       </Routes>
+      <Banner />
     </Router>
   );
 }
