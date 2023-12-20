12f916801d8336104d7b5e2e55b380183d7b83a2
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-20 12:13:54 -0800

    Add src/pages/Contact.js script

---
+++ src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "react-bootstrap/Container"
+
+
+const Contact = (props) => {
+
+  return (
+    <Container>
+      <div className="pt-5 d-flex justify-content-center">
+        <h2>Contact Us</h2>
+      </div>
+    </Container>
+  )
+}
+
+export default Contact
