68cc2b6c9e07c02171d117593fe757cb1431bba8
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-20 13:00:18 -0800

    import Banner to TeamControl

--- src/components/TeamControl.js
+++ src/components/TeamControl.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Team from '../pages/Team'
 import Stack from 'react-bootstrap/Stack'
+import Banner from "./Banner";
 
 function TeamControl() {
 
@@ -9,6 +10,7 @@ function TeamControl() {
       <Stack gap={2} className="p-4 col-md-8 mx-auto">
         <Team />
       </Stack>
+      <Banner />
     </React.Fragment>
   )
 }
