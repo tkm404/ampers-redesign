aaf8295625afff621c2bdb0b9c5e556b96dfd89d
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-20 12:12:58 -0800

    Add src/pages/Services.js script

---
+++ src/pages/Services.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Container from "react-bootstrap/Container"
+
+
+const Services = (props) => {
+
+  return (
+    <Container>
+      <div className="pt-5 d-flex justify-content-center">
+        <h2>Services</h2>
+      </div>
+      <div className="d-flex justify-content-center">
+        <h5>We do that, & that, &...</h5>
+      </div>
+    </Container>
+  )
+}
+
+export default Services
