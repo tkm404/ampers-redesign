a93cfe247e3af6bb417ca7f7e26595c1df6d6a02
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-21 10:18:24 -0800

    Replace carousel with code from medium.com bootstrap carousel

--- src/components/HomeCarousel.js
+++ src/components/HomeCarousel.js
@@ -1,29 +1,51 @@
+import React, {useState} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
+const data = [
+  {
+    image: require('../assets/img/examplepic1.jpg'),
+    caption: "Image Caption",
+    description: "Image Description"
+  },
+  {
+    image: require('../assets/img/examplepic2.jpg'),
+    caption: "Image Caption",
+    description: "Image Description"
+  },
+  {
+    image: require('../assets/img/examplepic3.jpg'),
+    caption: "Image Caption",
+    description: "Image Description"
+  }
+]
 
 function HomeCarousel() {
+
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <Carousel data-bs-theme="dark">
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h3>First Slide Label</h3>
-          <p>Peanut Butter Jelly Time</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h3>Second Slide Label</h3>
-          <p>Badger Badger Badger Badger...</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h3>Third Slide Label</h3>
-          <p>What Does the Fox Say?</p>
-        </Carousel.Caption>
-      </Carousel.Item>
+    <Carousel data-bs-theme="light" activeIndex={index} onSelect={handleSelect}>
+      {data.map((slide, i) => {
+        return (
+          <Carousel.Item>
+            <img
+              className='d-block w-100'
+              src={slide.image}
+              alt='slider'
+            />
+            <Carousel.Caption>
+              <h3>{slide.caption}</h3>
+              <p>{slide.description}</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        )
+      })}
+
     </Carousel>
-  )
+  );
 }
 
 export default HomeCarousel;
