ebc2ec98d661c02bc619d0034b74d1e1132df7c8
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-21 11:40:57 -0800

    Remove scaffold logo from App.js, remove Banner from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Header from './Header';
-import Banner from './Banner';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import logo from '../logo.svg';
 import './App.css';
 import HomeControl from './HomeControl';
 import ServicesControl from './ServicesControl'
 import TeamControl from './TeamControl'
 import ContactControl from './ContactControl'
+import Footer from './Footer';
 
 function App() {
   return (
@@ -20,6 +19,7 @@ function App() {
         <Route path='/contact' element={<ContactControl />} />
       </Routes>
       {/* <Banner /> */}
+      <Footer />
     </Router>
   );
 }
