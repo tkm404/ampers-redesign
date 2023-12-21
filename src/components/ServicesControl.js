55e477da555b4233b8b27134fd01318e15fcf91e
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2023-12-21 14:02:11 -0800

    import ServicesMenu to ServicesControl component

--- src/components/ServicesControl.js
+++ src/components/ServicesControl.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Services from '../pages/Services'
+import ServicesMenu from "./ServicesMenu";
 import Stack from 'react-bootstrap/Stack'
 import Banner from "./Banner";
 
@@ -7,12 +8,17 @@ import Banner from "./Banner";
 function ServicesControl() {
 
   return (
-    <React.Fragment>
-      <Stack gap={2} className="p-4 col-md-8 mx-auto">
-        <Services />
-      </Stack>
-      <Banner />
-    </React.Fragment>
+    <div> 
+      <React.Fragment>
+        <Stack gap={2} className="p-4 col-md-8 mx-auto">
+          <Services />
+        </Stack>
+        <Banner />
+      </React.Fragment>
+      <div>
+        <ServicesMenu />
+      </div>
+    </div>
   )
 }
 
