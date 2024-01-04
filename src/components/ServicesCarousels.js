60a181a49a5c96b63e1d6654e7509868bfaec920
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2024-01-04 10:43:31 -0800

    Add ServicesCarousel component to show slides on Services page

---
+++ src/components/ServicesCarousels.js
@@ -0,0 +1,104 @@
+import React, {useState} from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+const ResidentialData = [
+  {
+    image: require('../assets/img/servExample1.jpg'),
+  },
+  {
+    image: require('../assets/img/servExample2.jpg')
+  }
+]
+
+const CommercialData = [
+  {
+    image: require('../assets/img/servExample3.jpg')
+  },
+  {
+    image: require('../assets/img/servExample4.jpg')
+  }
+]
+
+const HandiworkData = [
+  {
+    image: require('../assets/img/servExample5.jpg')
+  },
+  {
+    image: require('../assets/img/servExample6.jpg')
+  }
+]
+
+function ResidentialCarousel() {
+
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel data-bs-theme="light" activeIndex={index} onSelect={handleSelect} fade>
+      {ResidentialData.map((slide, i) => {
+        return (
+          <Carousel.Item interval={null}>
+            <img
+            className='d-block w-100'
+            src={slide.image}
+            alt='slider'
+            />
+          </Carousel.Item>
+        )
+      })}
+    </Carousel>
+  )
+}
+
+function CommercialCarousel() {
+
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel data-bs-theme="light" activeIndex={index} onSelect={handleSelect} fade>
+      {CommercialData.map((slide, i) => {
+        return (
+          <Carousel.Item interval={null}>
+            <img
+            className='d-block w-100'
+            src={slide.image}
+            alt='slider'
+            />
+          </Carousel.Item>
+        )
+      })}
+    </Carousel>
+  )
+}
+
+function HandiworkCarousel() {
+
+  const [index, setIndex] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel data-bs-theme="light" activeIndex={index} onSelect={handleSelect} fade>
+      {HandiworkData.map((slide, i) => {
+        return (
+          <Carousel.Item interval={null}>
+            <img
+            className='d-block w-100'
+            src={slide.image}
+            alt='slider'
+            />
+          </Carousel.Item>          
+        )
+      })}
+    </Carousel>
+
+  )
+}
+
+export {ResidentialCarousel, CommercialCarousel, HandiworkCarousel}
