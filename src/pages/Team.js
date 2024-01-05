7d21b068f15066afa00fa3674f69ed9d823f54df
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2024-01-04 21:43:27 -0800

    Add message to Team page

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -9,6 +9,9 @@ const Team = (props) => {
       <div className="pt-5 d-flex justify-content-center">
         <h2>Meet Our Team</h2>
       </div>
+      <div className="d-flex justify-content-center">
+        <h6><em>Ampers & Ampersand</em> is a small company, born in Portland in 2015.  Since its inception, we've helped customers with projects big and small in the residential and commercial realms. As a testament to our friendly service and quality craftsmanship, many of our jobs are from return customers and their referrals. If you are finding us out of the blue, welcome to a best kept secret.</h6>
+      </div>
     </Container>
   )
 }
