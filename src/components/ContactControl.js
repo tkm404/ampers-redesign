81e109747c83a73196e773b894e793711acef00f
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2024-02-02 16:34:45 -0800

    Change ContactControl to handle back-end submits

--- src/components/ContactControl.js
+++ src/components/ContactControl.js
@@ -3,18 +3,32 @@ import Stack from 'react-bootstrap/Stack'
 import Contact from '../pages/Contact'
 import ContactForm from './ContactForm';
 import Banner from './Banner';
+import PropTypes from 'prop-types';
+import db from './../firebase.js'
+import { collection, addDoc } from 'firebase/firestore'
 
 
 function ContactControl() {
+
+  const handleAddingClientSubmission = async (newClientMessage) => {
+    const collectionRef = collection(db, "TestEmails");
+    await addDoc(collectionRef, newClientMessage);
+  }
+
   return (
     <React.Fragment>
       <Stack gap={2} className="p-4 col-md-8 mx-auto">
         <Contact />
       </Stack>
       <Banner />
-      {/* <ContactForm/> */}
+      <ContactForm
+      onClientSubmission={handleAddingClientSubmission}/>
     </React.Fragment>
   )
+}
+
+ContactControl.propTypes = {
+  onClientSubmission: PropTypes.func
 }
 
 export default ContactControl
