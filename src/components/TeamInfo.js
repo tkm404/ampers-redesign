6aeced9426af54615529d4535c1d6577363035b8
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2024-02-05 10:18:03 -0800

    remove unnec. Banner import from TeamInfo, add semicolons

--- src/components/TeamInfo.js
+++ src/components/TeamInfo.js
@@ -1,8 +1,7 @@
 import React from "react";
-import Banner from "./Banner";
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
-import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 
 
 function TeamInfo() {
