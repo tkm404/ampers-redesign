435a24b13245f25c9680d220d7b374342693123f
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2024-02-05 10:19:47 -0800

    add visual state handling in ContactControl

--- src/components/ContactControl.js
+++ src/components/ContactControl.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Stack from 'react-bootstrap/Stack';
 import Contact from '../pages/Contact';
 import ContactForm from './ContactForm';
@@ -6,25 +6,63 @@ import Banner from './Banner';
 import PropTypes from 'prop-types';
 import { db } from './../firebase.js';
 import { collection, addDoc } from 'firebase/firestore';
+import ContactSuccess from './ContactSuccess.js';
+import ContactError from './ContactError.js'
 
 
 function ContactControl() {
 
+  const [formVisibleOnPage, setFormVisibleOnPage ] = useState(true);
+  const [thankYouSubmit, setThankYouSubmit] = useState(false)
+  const [error, setError] = useState(null);
+
   const handleAddingClientSubmission = async (newClientMessage) => {
     const collectionRef = collection(db, "TestEmails");
     await addDoc(collectionRef, newClientMessage);
+    setFormVisibleOnPage(false)
+    if (!error) {
+      setThankYouSubmit(true)
+    } else {
+      setError(error)
+    }
   }
 
+    let currentlyVisible = null;  
+
+  if (formVisibleOnPage) {
   return (
     <React.Fragment>
       <Stack gap={2} className="p-4 col-md-8 mx-auto">
         <Contact />
       </Stack>
       <Banner />
+      
       <ContactForm
       onClientSubmission={handleAddingClientSubmission}/>
     </React.Fragment>
-  )
+  )    
+  } else if (thankYouSubmit) {
+    return (
+      <React.Fragment>
+        <Stack gap={2} className="p-4 col-md-8 mx-auto">
+          <Contact />
+        </Stack>
+        <Banner />
+        <ContactSuccess/>
+      </React.Fragment>
+    )    
+  } else {
+    return (
+      <React.Fragment>
+        <Stack gap={2} className="p-4 col-md-8 mx-auto">
+          <Contact />
+        </Stack>
+        <Banner />
+        <ContactError/>
+      </React.Fragment>
+    )
+  }
+
 }
 
 ContactControl.propTypes = {
