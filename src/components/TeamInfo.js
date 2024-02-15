d2fecdf997c9e7c74c21753559a2ec30b814d925
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2024-02-15 10:49:14 -0800

    replace placeholder team photos with team group photo

--- src/components/TeamInfo.js
+++ src/components/TeamInfo.js
@@ -2,23 +2,29 @@ import React from "react";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import AmpersTeam1 from '../assets/img/AmpersTeam1.jpg'
 
 
 function TeamInfo() {
 
   return (
     <Container>
-      <Row className="p-4 mx-auto">
-        <Col>
-          <img alt="Scott goes here"></img>
-          <h6>Scott, Owner</h6>
+      <Row className=" d-flex justify-content-center">
+        <Col xs={8}>
+          <img id="teamPhoto" src={AmpersTeam1} alt="The Ampers & Ampersand Team"></img>
         </Col>
-        <Col>
+        <div className="d-flex justify-content-center">
+          <h6>Top Row: The Ampers Team</h6>
+        </div>
+        <div className="d-flex justify-content-center">       
+          <h6>Bottom Row: Scott, Owner; Jesse, Site Lead</h6>             
+        </div>
+        {/* <Col>
           <img alt="Igor is the Ops Man"></img>        
           <h6>Igor, Operations Manager</h6>
-        </Col>
+        </Col> */}
       </Row>
-      <Row className="p-4 mx-auto">
+      {/* <Row className="p-4 mx-auto">
         <Col>
           <img alt="Jesse Site Lead"></img>
           <h6>Jesse, Site Lead</h6>
@@ -41,7 +47,7 @@ function TeamInfo() {
       <Row className="p-4 mx-auto">
         <img alt="Matt Crewmember"></img>
         <h6>Matt, Crewmember</h6>
-      </Row>
+      </Row> */}
     </Container>
   )
 }
