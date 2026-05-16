9de5b618799f9f455ef4cf73f254776cf805241c
Thomas McDowell <127699298+tkm404@users.noreply.github.com>
2026-05-16 11:17:21 -0700

    WIP: add state data to card indexes in customer reviews

--- src/components/CustomerReviewCarousel.js
+++ src/components/CustomerReviewCarousel.js
@@ -26,6 +26,17 @@ const CardItems = [
   },
 ];
 
+function determineClasses(indexes, cardIndex) {
+  if (indexes.currentIndex === cardIndex) {
+    return 'active';
+  } else if (indexes.nextIndex === cardIndex) {
+    return 'next';
+  } else if (indexes.previousIndex === cardIndex) {
+    return 'prev';
+  } 
+  return 'inactive';
+}
+
 const CardCarousel = () => {
   const [indexes, setIndexes] = useState({
     previousIndex: 0,
